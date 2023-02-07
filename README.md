75e7cb7194ad6e365fdb8fc7c2dc6907dcda8b23
herculesgimenes <herculesggimenes@gmail.com>
2023-02-07 08:53:28 -0300

    :sparkles: tests

---
+++ README.md
@@ -0,0 +1,9 @@
+Kotlin Bazel Monorepo
+
+some commands
+"""
+bazel run //java/server
+bazel run //java/client
+bazel test //java_tests/service
+
+"""
--- java/server/Server.kt
+++ java/server/Server.kt
@@ -1,7 +1,12 @@
 package java.server
 
 class Server {
-    fun test (): String {
-        return "Hello World"
+    fun test (name: String): String {
+        return "Hello $name"
     }
+}
+
+fun main(){
+    val server = Server()
+    server.test("World")
 }
